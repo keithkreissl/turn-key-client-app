18ac8efb8a35a503396465ca5408c76e78a5c4db
Keith <keithkreissl@gmail.com>
2015-04-01 21:03:57 -0500

    adding logback and logback.groovy config file

---
+++ src/main/java/turn/key/execute/Execute.java
@@ -0,0 +1,16 @@
+package turn.key.execute;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Execute {
+	private static final Logger logger = LoggerFactory.getLogger(Execute.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		logger.debug("this is a message");
+
+	}
+
+}
