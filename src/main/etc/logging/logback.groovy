7f2986d0f10450de6253e9f6402524b821d1c492
Keith Kreissl <kkreissl@cars.com>
2015-04-02 09:44:47 -0500

    got logback configuration to work when using application plugin

--- src/main/etc/logging/logback.groovy
+++ src/main/etc/logging/logback.groovy
@@ -1,8 +1,9 @@
 appender("CONSOLE", ConsoleAppender){
-	println "i am in the groovy file"
 	encoder(PatternLayoutEncoder){
 		pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
    	 }
 }
 
 root(ERROR, ["CONSOLE"])
+
+logger("turn.key.execute", INFO)
